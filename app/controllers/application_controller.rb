574792a21b6224e7349fb9ddb66165f8cfc489bd
MeterSoft <voinbpy@mail.ru>
2012-12-23 21:36:50 +0200

    Refactor all to working

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,25 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+
+  before_filter :authenticate_user!
+
+  layout :layout_by_resource
+
+  protected
+
+  def after_sign_out_path_for(resource_or_scope)
+    #new_user_session_path
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    #new_user_session_path
+  end
+
+  def layout_by_resource
+    if devise_controller?
+      "login"
+    else
+      "application"
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Net::Application.config.secret_token = '2a7c7ae8f43e6f91b7980975ff2e0d7bb3d09220230139f3b6aad9b1c6f76bbd717a49f18fbaa675b0912d9c2ae7d261af4195ac159abfcd8ce2703a6482fd31'
