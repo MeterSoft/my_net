5f39fde413749d467df22c4ad82ee9f529591c1e
Meter <meter@meter-System-Product-Name.(none)>
2012-12-25 15:27:50 +0200

    new

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Net::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   #config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { :host => 'http://outerspace.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'outerspace.herokuapp.com' }
   config.action_mailer.smtp_settings = {
       :address => "smtp.gmail.com",
       :port => 587,
