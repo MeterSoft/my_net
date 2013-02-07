c2d35fcf5e8bcca95923af34d73dbcdc77c0e26f
Semenets Bohdan <voinboy@mail.ru>
2013-02-07 15:35:43 +0200

    add crop

--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -9,8 +9,12 @@ class UserProfileController < ApplicationController
 
     respond_to do |format|
       if @user.update_attributes(params[:user])
-        format.html { redirect_to main_page_index_path, notice: 'Category was successfully updated.' }
-        format.json { head :no_content }
+        if params[:user][:avatar].blank?
+          format.html { redirect_to main_page_index_path, notice: 'Category was successfully updated.' }
+          format.json
+        else
+          format.html { render :action => "crop" }
+        end
       else
         format.html { render action: "edit" }
         format.json { render json: @user.errors, status: :unprocessable_entity }
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,19 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#", :msg => "15x15#" },
+  has_attached_file :avatar,
+                    :styles => { :medium => {:geometry => "300x300>" },
+                                 :small => {:geometry => "100x100#", :processors => [:cropper]},
+                                 :large => {:geometry => "600x600>" } },
                     :default_url => 'default_large.png'
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me,
-                  :first_name, :last_name, :thread_name, :avatar, :time, :status
+                  :first_name, :last_name, :thread_name, :avatar, :time, :status,
+                  :crop_x, :crop_y, :crop_w, :crop_h
+
+  attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
+  after_update :reprocess_avatar, :if => :cropping?
 
   has_many :friends, :foreign_key => "user_id", :class_name => "Friend"
   has_many :inverse_friends, :foreign_key => "user_friend_id", :class_name => "Friend"
@@ -27,5 +34,20 @@ class User < ActiveRecord::Base
   def friend?(current_user)
     Friend.find_by_user_id_and_user_friend_id_and_status(self, current_user, 'confirmed') ||
     Friend.find_by_user_id_and_user_friend_id_and_status(current_user, self, 'confirmed')
+  end
+
+  def cropping?
+    !crop_x.blank? && !crop_y.blank? && !crop_w.blank? && !crop_h.blank?
+  end
+
+  def avatar_geometry(style = :original)
+    @geometry ||= {}
+    @geometry[style] ||= Paperclip::Geometry.from_file(avatar.path(style))
+  end
+
+  private
+
+  def reprocess_avatar
+    avatar.reprocess!
   end
 end
