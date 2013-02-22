c2d6bf889fac74b9105fcb4ed10b89bbd188ae9e
metersoft <voinboy@mail.ru>
2013-02-22 18:19:53 +0200

    fix

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -1,9 +1,5 @@
 class MainPageController < ApplicationController
 
-  def index
-     @posters = current_user.posters.all
-  end
-
   def show
     @user = User.find(params[:id])
     @posters = @user.posters.all
