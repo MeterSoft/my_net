e2e89b1814c77813a33d36ebd86ae8b62d4e896a
metersoft <voinboy@mail.ru>
2013-02-22 18:07:06 +0200

    fix

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -7,11 +7,11 @@ class FriendsController < ApplicationController
     if params[:user_id]
       @friend = Friend.new(:user_id => current_user.id, :user_friend_id => params[:user_id], :status => 'invite')
       if @friend.save
-        redirect_to main_page_index_path
+        redirect_to :back
       end
     else
       Friend.find(params[:user_confirmed_id]).update_attributes(:status => "confirmed")
-      redirect_to main_page_index_path
+      redirect_to :back
     end
   end
 
