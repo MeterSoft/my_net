7e4fc4d65216d9a2001290252ac8867da31a7395
metersoft <voinboy@mail.ru>
2013-02-23 17:12:51 +0200

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130222124335) do
+ActiveRecord::Schema.define(:version => 20130222135439) do
 
   create_table "conversations", :force => true do |t|
     t.string   "subject",    :default => ""
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(:version => 20130222124335) do
   create_table "posters", :force => true do |t|
     t.integer  "user_id"
     t.text     "message"
-    t.datetime "send"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
