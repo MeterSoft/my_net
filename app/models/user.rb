a554684d1656571519d4e0e0d382b810298ee790
Aleksey Verbivskyy <verbaleks@gmail.com>
2013-03-11 15:13:15 +0200

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -67,16 +67,19 @@ class User < ActiveRecord::Base
 
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     #user = User.where(:provider => auth.provider, :uid => auth.uid).first
-    user = User.find_by_provider_and_uid_or_email(auth.provider, auth.uid, auth.info.email)
+    user = User.find_by_provider_and_uid(auth.provider, auth.uid)
     unless user
-      user = User.create(first_name: auth.extra.raw_info.first_name,
-                         last_name: auth.extra.raw_info.last_name,
-                         provider: auth.provider,
-                         uid: auth.uid,
-                         email: auth.info.email,
-                         password: Devise.friendly_token[0,20],
-                         avatar: open(auth.info.image)
-      )
+      user = User.find_by_email(auth.info.email)
+      unless user
+        user = User.create(first_name: auth.extra.raw_info.first_name,
+                           last_name: auth.extra.raw_info.last_name,
+                           provider: auth.provider,
+                           uid: auth.uid,
+                           email: auth.info.email,
+                           password: Devise.friendly_token[0,20],
+                           avatar: open(auth.info.image)
+        )
+      end
     end
     user
   end
