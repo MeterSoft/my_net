69f30ecd3b9765b080e57b68090f6a48d8b6798d
MeterSoft <metersoft@ubuntu.(none)>
2013-05-26 23:21:58 +0300

    add check user profile permision

--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -1,12 +1,11 @@
 class UserProfileController < ApplicationController
+  before_filter :profile, only: [:show, :edit, :update]
+  before_filter :check_permissions, only: [:edit, :update]
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
-
     respond_to do |format|
       if @user.update_attributes(params[:user])
         format.html { redirect_to main_page_path(current_user.id) }
@@ -20,5 +19,15 @@ class UserProfileController < ApplicationController
 
   def subregion_options
     render partial: 'subregion_select'
+  end
+
+  private
+
+  def profile
+    @user = User.find(params[:id])
+  end
+
+  def check_permissions
+    redirect_to root_path if @user != current_user
   end
 end
