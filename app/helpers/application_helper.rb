786f22ab73362b794d4afe709e0a58d4ea5d5eb3
meter <meter@semenets.(none)>
2013-06-13 14:39:11 +0300

    fixed autodetected link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   REGEXP_URL = /(\s|^)(((https?|ftp)\:\/\/)?([a-z0-9]{1})((\.[a-z0-9-])|([a-z0-9-]))*[\.:]([a-z0-9]{2,4})(\/[^\s]{0,})?)/i
-  URL_POSITION = 2
+  URL_POSITION = 1
   JS_NOOP = "javascript:void(false);"
 
   def convert_link(url)
@@ -8,9 +8,9 @@ module ApplicationHelper
   end
 
   def auto_detected_link(description)
-    description.match(REGEXP_URL) do |url|
-      description.sub!(url[URL_POSITION], "<a href='#{convert_link(url[URL_POSITION])}' rel='nofollow' target='_blank'>#{url[URL_POSITION]}</a>")
-    end
+    description.gsub!(REGEXP_URL) do |url|  
+      "<a href='#{convert_link(url)}' rel='nofollow' target='_blank'>#{url}</a>"
+    end  
     description.gsub(/\r\n/, "<br>").html_safe
   end
 end
