1292fdd4f35b4960b0d53152236a1851b75f7b0a
MeterSoft <metersoft@ubuntu.(none)>
2013-06-16 15:25:51 +0300

    new update user status and refactor regexp link detected

--- app/controllers/messages_counts_controller.rb
+++ app/controllers/messages_counts_controller.rb
@@ -1,13 +1,17 @@
 class MessagesCountsController < ApplicationController
+  skip_before_filter :lenguage
 
   def index
-    @invite = current_user.pending_inverse_friends.count
-    @invite = nil if @invite == 0
-    @count = current_user.receipts.where(is_read: false, receiver_id: current_user.id).count if current_user
-    @count = nil if @count == 0
-    respond_to do |format|
-      format.json { render json: {count: @count, invite: @invite} }
-    end
+  	if current_user
+	  	current_user.update_attributes(time: Time.now)
+	    @invite = current_user.pending_inverse_friends.count
+	    @invite = nil if @invite == 0
+	    @count = current_user.receipts.where(is_read: false, receiver_id: current_user.id).count 
+	    @count = nil if @count == 0
+	    respond_to do |format|
+	      format.json { render json: {count: @count, invite: @invite} }
+	    end
+	end
   end
 
 end
